e9705d1eba57ac5eb50e809bf807a8e0ed0d480e
Lukas Fritz <lukas.fritz@student.tugraz.at>
2019-11-26 14:29:45 +0200

    added cubic operation mode

--- cubic2equi-master/own_cubic2equi_demo.m
+++ cubic2equi-master/own_cubic2equi_demo.m
@@ -1,11 +1,11 @@
-left = hdrimread('cube_left.hdr');
-right = hdrimread('cube_right.hdr');
-top = hdrimread('cube_top.hdr');
-bottom = hdrimread('cube_bottom.hdr');
-front = hdrimread('cube_front.hdr');
-back = hdrimread('cube_back.hdr');
+left = hdrimread('CreatedHDRTonemapped_cube_left2.jpg');
+right = hdrimread('CreatedHDRTonemapped_cube_right2.jpg');
+top = hdrimread('CreatedHDRTonemapped_cube_top2.jpg');
+bottom = hdrimread('CreatedHDRTonemapped_cube_bottom2.jpg');
+front = hdrimread('CreatedHDRTonemapped_cube_front2.jpg');
+back = hdrimread('CreatedHDRTonemapped_cube_back2.jpg');
 
 close all;
 out = own_cubic2equi(top, bottom, left, right, front, back);
 imshow(out);
-hdrimwrite(out, 'combined_cube.hdr');
+hdrimwrite(out, 'CreatedHDRTonemapped_combined_cube2.jpg');
--- cubic2equi-master/own_equi2cubic_demo.m
+++ cubic2equi-master/own_equi2cubic_demo.m
@@ -1,6 +1,6 @@
 % Read in the image
-equi = hdrimread('pano1.hdr');
-
+%equi = hdrimread('pano_end.hdr');
+equi = imread('panos-00617.jpg');
 % Close all figures and create cubic images
 close all;
 out = own_equi2cubic(equi, 1920); % Set to 500 x 500
@@ -8,27 +8,30 @@ out = own_equi2cubic(equi, 1920); % Set to 500 x 500
 % Now show all of the cube faces as a demonstration
 names = {'Front Face', 'Right Face', 'Back Face', 'Left Face', ...
     'Top Face', 'Bottom Face'};
-names_to_save = {'cube_front.hdr', 'cube_right.hdr', 'cube_back.hdr', ...
-    'cube_left.hdr', 'cube_top.hdr', 'cube_bottom.hdr'};
+%names_to_save = {'cube_front617.hdr', 'cube_right617.hdr', 'cube_back617.hdr', ...
+%    'cube_left617.hdr', 'cube_top617.hdr', 'cube_bottom617.hdr'};
+names_to_save = {'cube_front617.jpg', 'cube_right617.jpg', 'cube_back617.jpg', ...
+    'cube_left617.jpg', 'cube_top617.jpg', 'cube_bottom617.jpg'};
 
 % Show original equirectangular image
-figure;
-imshow(equi);
-title('Equirectangular Image');
+%figure;
+%imshow(equi);
+%title('Equirectangular Image');
 
 % Show the cube faces and also write them to disk
-figure;
+%figure;
 for idx = 1 : numel(names)
     % Show image in figure and name the face
     subplot(2,3,idx);
     imshow(out{idx});
     title(names{idx});
     % Write the image to disk
-    hdrimwrite(out{idx}, names_to_save{idx});
+   % hdrimwrite(out{idx}, names_to_save{idx});
+    imwrite(out{idx}, names_to_save{idx});
 end
 
 % Show a montage
-mont_image = horzcat(out{:});
-figure;
-imshow(mont_image);
-title('Montage - Front, Right, Back, Left, Top, Bottom');
+%mont_image = horzcat(out{:});
+%figure;
+%imshow(mont_image);
+%title('Montage - Front, Right, Back, Left, Top, Bottom');
